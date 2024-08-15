8cba52aac53bc974cebe9a6d8c2b88f3e1247e9d
Facundo <marcofacundolucas@gmail.com>
2024-08-15 14:56:21 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { DoctorProvider } from "./context/DoctorContext.jsx";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { DoctorProvider } from "./context/DoctorContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
